1e8d8e834ce09ae2b46886e7ef1e90df33bbf8b3
Skylar Kelty <s.kelty@kent.ac.uk>
2015-09-07 14:01:21 +0100

    Fix 3.0 missing component

--- version.php
+++ version.php
@@ -1,8 +1,22 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015060500;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2015090700;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2012110900; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Mass Actions Block';
+$plugin->component = 'block_massaction';
